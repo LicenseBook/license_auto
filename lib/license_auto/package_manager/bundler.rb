27f271fbd5fc0e771520d65a2269fed495ee8110
yyqinfo <yyqinfo@163.com>
2016-07-18 17:40:58 +0800

    if no gemfile.lock,then invoke 'bundle install' to generate gemfile.lock

--- lib/license_auto/package_manager/bundler.rb
+++ lib/license_auto/package_manager/bundler.rb
@@ -29,6 +29,19 @@ module LicenseAuto
       # }
 
       lock_files = dependency_file_path_names
+      gem_files = dependency_file_path_names(pattern=gemfile_pattern)
+      if lock_files.empty? && !gem_files.empty?
+        env_bundle_gemfile, ENV["BUNDLE_GEMFILE"] = ENV["BUNDLE_GEMFILE"], nil
+        Dir.chdir(@path) do
+          cmd = 'bundle install'
+          stdout_str, stderr_str, _status = Open3.capture3(cmd)
+          LicenseAuto.logger.debug(stdout_str) if stdout_str
+          LicenseAuto.logger.error(stderr_str) if stderr_str
+        end
+        ENV["BUNDLE_GEMFILE"] = env_bundle_gemfile
+      end
+
+      lock_files = dependency_file_path_names
       if lock_files.empty?
         LicenseAuto.logger.info("#{LANGUAGE}: #{dependency_file_pattern} file not exist")
         gem_files = dependency_file_path_names(pattern=gemfile_pattern)
@@ -38,47 +51,47 @@ module LicenseAuto
         end
         return []
       else
-        lock_files.map {|dep_file|
+        lock_files.map { |dep_file|
           LicenseAuto.logger.debug(dep_file)
           lockfile_parser = ::Bundler::LockfileParser.new(::Bundler.read_file(dep_file))
           {
               dep_file: dep_file,
-              deps: lockfile_parser.specs.map {|spec|
-                      remote =
-                          case
-                            when spec.source.class == ::Bundler::Source::Git
-                              spec.source.uri
-                            when spec.source.class == ::Bundler::Source::Rubygems
-                              if spec.source.remotes.size == 1
-                                spec.source.remotes.first.to_s
-                              elsif spec.source.remotes.size >= 1
-                                # remotes =
-                                #     if Gems.info(spec.name) == RubyGemsOrg::GEM_NOT_FOUND
-                                #       spec.source.remotes.reject {|uri|
-                                #         uri.to_s == RubyGemsOrg::URI
-                                #       }
-                                #     else
-                                #       spec.source.remotes
-                                #     end
-                                # TODO: support http://www.gemfury.com, aka multi `source` DSL; requre 'rubygems'?
-                                spec.source.remotes.map {|r|
-                                  r.to_s
-                                }.join(',')
-                              end
-                            when spec.source.class == ::Bundler::Source::Path::Installer
-                              # Untested
-                              spec.full_gem_path
-                            when spec.source.class == ::Bundler::Source::Path
-                              "."
-                            else
-                              raise('Yo, this error should ever not occur!')
-                          end
-                      {
-                          name: spec.name,
-                          version: spec.version.to_s,
-                          remote: remote
-                      }
+              deps: lockfile_parser.specs.map { |spec|
+                remote =
+                    case
+                      when spec.source.class == ::Bundler::Source::Git
+                        spec.source.uri
+                      when spec.source.class == ::Bundler::Source::Rubygems
+                        if spec.source.remotes.size == 1
+                          spec.source.remotes.first.to_s
+                        elsif spec.source.remotes.size >= 1
+                          # remotes =
+                          #     if Gems.info(spec.name) == RubyGemsOrg::GEM_NOT_FOUND
+                          #       spec.source.remotes.reject {|uri|
+                          #         uri.to_s == RubyGemsOrg::URI
+                          #       }
+                          #     else
+                          #       spec.source.remotes
+                          #     end
+                          # TODO: support http://www.gemfury.com, aka multi `source` DSL; requre 'rubygems'?
+                          spec.source.remotes.map { |r|
+                            r.to_s
+                          }.join(',')
+                        end
+                      when spec.source.class == ::Bundler::Source::Path::Installer
+                        # Untested
+                        spec.full_gem_path
+                      when spec.source.class == ::Bundler::Source::Path
+                        "."
+                      else
+                        raise('Yo, this error should ever not occur!')
+                    end
+                {
+                    name: spec.name,
+                    version: spec.version.to_s,
+                    remote: remote
                 }
+              }
           }
         }
       end
